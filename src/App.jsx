b45b4ace81eacf841d4e45b0cfde77823e913d57
miguel Romeroe <makonisud1@gmail.com>
2026-07-15 10:42:54 -0500

    Gestion de usuarios y reporte de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 //📌 Enrutador centralizado con react-router-dom y envoltura de contextos
 
-import FormularioReporte from './components/FormularioReporte';
+
+import UsersListPage from "./pages/admin/UsersListPage";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-4">
-      <FormularioReporte /> {/* Esto solo es momentanio */}
+      
+      <UsersListPage></UsersListPage>
     </div>
   );
 }
